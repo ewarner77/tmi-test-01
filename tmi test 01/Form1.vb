6530e5a7355198fe884a6b99c56c07b5dc2f28c7
Eric Warner <eric@warner-consulting.com>
2018-11-08 00:32:38 -0800

    changed on desktop

--- tmi test 01/Form1.vb
+++ tmi test 01/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Text = "changed program"
     End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
 End Class
